1a17c794fba3df2e5b01122fa65b84bdd7978c3a
Nika Sommer <nika.sommer@stud.htwk-leipzig.de>
2025-05-23 12:45:46 +0200

    feat: add NavBar

--- app/src/main/java/org/htwk/pacing/MainActivity.kt
+++ app/src/main/java/org/htwk/pacing/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import org.htwk.pacing.screens.*
 import org.htwk.pacing.ui.theme.PacingTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +33,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PacingTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val navController = rememberNavController()
+                val startDestination = Destination.HOME
+                val selectedDestination =
+                    rememberSaveable { mutableIntStateOf(startDestination.ordinal) }
+
+                Scaffold(
+                    bottomBar = { NavBar(navController, selectedDestination) },
+                ) { contentPadding ->
+                    AppNavHost(
+                        navController,
+                        startDestination,
+                        modifier = Modifier.padding(contentPadding)
                     )
                 }
             }
@@ -31,17 +53,60 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun NavBar(
+    navController: NavHostController,
+    selectedDestination: MutableState<Int>,
+    modifier: Modifier = Modifier
+) {
+    NavigationBar(windowInsets = NavigationBarDefaults.windowInsets) {
+        Destination.entries.forEachIndexed { index, destination ->
+            NavigationBarItem(
+                selected = selectedDestination.value == index,
+                onClick = {
+                    navController.navigate(route = destination.route)
+                    selectedDestination.value = index
+                },
+                icon = {
+                    Icon(
+                        destination.icon,
+                        contentDescription = destination.contentDescription
+                    )
+                },
+                label = { Text(destination.label) }
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+enum class Destination(
+    val route: String,
+    val label: String,
+    val icon: ImageVector,
+    val contentDescription: String
+) {
+    HOME("home", "Home", Icons.Rounded.Home, "Home"),
+    MEASUREMENTS("measurements", "Measurements", Icons.Rounded.Home, "Measurements"),
+    SETTINGS("settings", "Settings", Icons.Rounded.Settings, "Settings")
+}
+
 @Composable
-fun GreetingPreview() {
-    PacingTheme {
-        Greeting("Android")
+fun AppNavHost(
+    navController: NavHostController,
+    startDestination: Destination,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController,
+        startDestination = startDestination.route
+    ) {
+        Destination.entries.forEach { destination ->
+            composable(destination.route) {
+                when (destination) {
+                    Destination.HOME -> HomeScreen()
+                    Destination.MEASUREMENTS -> MeasurementsScreen()
+                    Destination.SETTINGS -> SettingsScreen()
+                }
+            }
+        }
     }
 }
