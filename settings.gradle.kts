af1c7afd22a320523a51c2dcda838434ac2b925a
Paul Köhler <paul.koehler@stud.htwk-leipzig.de>
2025-05-28 16:58:26 +0200

    chore: init and add gitlab-ci

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    plugins {
+        id("org.sonarqube") version "6.2.0.5505"
+    }
+
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ui"
+include(":app")
+ 
