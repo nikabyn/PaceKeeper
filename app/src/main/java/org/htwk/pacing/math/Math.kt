68cc93346fecb65d8377dc0601dd4e9d04160894
Nika Sommer <nika.sommer@stud.htwk-leipzig.de>
2025-06-04 15:49:48 +0200

    feat #1: add error case for remap

--- app/src/main/java/org/htwk/pacing/math/Math.kt
+++ app/src/main/java/org/htwk/pacing/math/Math.kt
@@ -5,5 +5,9 @@ fun interpolate(a: Float, b: Float, t: Float): Float {
 }
 
 fun remap(value: Float, lowIn: Float, highIn: Float, lowOut: Float, highOut: Float): Float {
+    if (lowIn == highIn) {
+        println("Error: Invalid input range for remap: lowIn == highIn")
+        return 0f
+    }
     return lowOut + (value - lowIn) * (highOut - lowOut) / (highIn - lowIn)
 }
