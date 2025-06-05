ad5d95448d30a085d9b49d000865f14ea8baac66
Matthias Zupp <matthias.zupp@stud.htwk-leipzig.de>
2025-06-05 19:22:53 +0200

    feat #2: turn battery

--- app/src/main/java/org/htwk/pacing/ui/components/Battery.kt
+++ app/src/main/java/org/htwk/pacing/ui/components/Battery.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.layout.layout
@@ -14,13 +15,20 @@ import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun BatterieKomponente() {
+    Box(
+        modifier = Modifier
+            .graphicsLayer(rotationZ = 90f)
+    ) {
+        BatterieInhalt()
+    }
+}
+@Composable
+fun BatterieInhalt() {
     val overlap = 18.dp
 
     Box(
         modifier = Modifier.size(width = 100.dp, height = 200.dp)
     ) {
-
-
         // oben
         Box(
             modifier = Modifier
@@ -45,6 +53,7 @@ fun BatterieKomponente() {
         )
     }
 }
+
 
 fun Modifier.dynamicHeightMinus(overlap: Dp) = this.then(
     layout { measurable, constraints ->
