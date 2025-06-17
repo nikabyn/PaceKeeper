4cec8f46ff4ddf5b7d288fb8489acbb022449abb
Maximilian Schaldach <maximilian.schaldach@stud.htwk-leipzig.de>
2025-06-17 21:23:10 +0200

    feat: #2 add missing imports

--- app/src/main/java/org/htwk/pacing/ui/screens/Home.kt
+++ app/src/main/java/org/htwk/pacing/ui/screens/Home.kt
@@ -3,11 +3,13 @@ package org.htwk.pacing.ui.screens
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 import kotlinx.datetime.Clock
 import kotlin.time.Duration.Companion.hours
