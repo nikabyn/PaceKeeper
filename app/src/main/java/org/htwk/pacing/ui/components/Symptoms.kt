841bbdd69a7fec54b78e880301544e9653d776a2
Emilia Mühmel <emilia.muehmel@stud.htwk-leipzig.de>
2025-06-26 20:55:55 +0200

    feat: #14 add symptoms checkboxes

--- app/src/main/java/org/htwk/pacing/ui/components/Symptoms.kt
+++ app/src/main/java/org/htwk/pacing/ui/components/Symptoms.kt
@@ -1,13 +1,16 @@
 package org.htwk.pacing.ui.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -17,6 +20,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
@@ -68,7 +76,13 @@ fun SymptomScreen(navController: NavController) {
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary,
                 ),
-                title = { Text("Select your symptoms") },
+                title = {
+                    Column {
+                        Text("Symptoms", style = MaterialTheme.typography.titleLarge)
+                        Text("Select your symptoms", style = MaterialTheme.typography.titleSmall)
+                    }
+
+                },
                 navigationIcon = {
                     IconButton(onClick = { navController.navigate(Route.Home) }) {
                         Icon(
@@ -80,10 +94,35 @@ fun SymptomScreen(navController: NavController) {
 
             )
         }) { contentPadding ->
-        Box(
+        Column(
             modifier = Modifier
                 .padding(contentPadding)
                 .fillMaxSize()
-        ) {}
+        ) {
+            CheckboxMinimalExample("Headache")
+            CheckboxMinimalExample("Back pain")
+            CheckboxMinimalExample("Nausea")
+            CheckboxMinimalExample("Exhaustion")
+        }
+    }
+}
+
+@Composable
+fun CheckboxMinimalExample(title: String) {
+    var checked by remember { mutableStateOf(false) }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .clickable(onClick = { checked = !checked })
+            .fillMaxWidth()
+    ) {
+        Checkbox(
+            checked = checked,
+            onCheckedChange = { checked = it }
+        )
+        Text(
+            title
+        )
     }
 }
