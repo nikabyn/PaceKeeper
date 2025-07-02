4dba9fec174f64e14b340b52dfc515babf5b71f3
Nika Sommer <nika.sommer@stud.htwk-leipzig.de>
2025-07-02 15:14:51 +0200

    feat #14: check for empty symptom names

--- app/src/main/java/org/htwk/pacing/ui/components/Symptoms.kt
+++ app/src/main/java/org/htwk/pacing/ui/components/Symptoms.kt
@@ -6,19 +6,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -39,9 +34,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavController
 import org.htwk.pacing.R
 import org.htwk.pacing.ui.Route
@@ -85,7 +78,6 @@ fun SymptomSelectionCard(navController: NavController) {
 @Composable
 fun SymptomScreen(navController: NavController) {
     var openDialog by remember { mutableStateOf(false) }
-    var newSymptom by remember { mutableStateOf("") }
 
     var symptoms = remember { mutableListOf("Headache", "Exhaustion", "Nausea", "Back pain") }
 
@@ -99,9 +91,9 @@ fun SymptomScreen(navController: NavController) {
                 title = {
                     Row(horizontalArrangement = Arrangement.SpaceBetween) {
                         Column {
-                            Text("Symptoms", style = MaterialTheme.typography.titleLarge)
+                            Text("Select Symptoms", style = MaterialTheme.typography.titleLarge)
                             Text(
-                                "Select your symptoms",
+                                "Why are you feeling really bad?",
                                 style = MaterialTheme.typography.titleSmall
                             )
                         }
@@ -121,7 +113,6 @@ fun SymptomScreen(navController: NavController) {
                         Text("Apply")
                     }
                 }
-
             )
         },
         floatingActionButton = {
@@ -140,39 +131,16 @@ fun SymptomScreen(navController: NavController) {
         ) {
             symptoms.forEach { SymptomCheckBox(it) }
         }
+
         if (openDialog) {
-            AlertDialog(
-                onDismissRequest = {
+            AddSymptomDialog(
+                onCancel = {
                     openDialog = false
                 },
-                title = {
-                    Text(text = "Add symptom")
-                },
-                text = {
-                    TextField(
-                        value = newSymptom,
-                        onValueChange = { newSymptom = it }
-                    )
-                },
-                confirmButton = {
-                    Button(
-                        onClick = {
-                            openDialog = false
-                            symptoms.add(newSymptom)
-                            newSymptom = ""
-                        }
-                    ) {
-                        Text("Confirm")
-                    }
-                },
-                dismissButton = {
-                    TextButton(
-                        onClick = { openDialog = false }
-                    ) {
-                        Text("Dismiss")
-                    }
-                }
-            )
+                onConfirm = { newSymptom ->
+                    openDialog = false
+                    symptoms.add(newSymptom)
+                })
         }
     }
 }
@@ -195,4 +163,43 @@ fun SymptomCheckBox(title: String) {
             title
         )
     }
+}
+
+@Composable
+fun AddSymptomDialog(
+    onCancel: () -> Unit,
+    onConfirm: (newSymptom: String) -> Unit,
+) {
+    var newSymptom by remember { mutableStateOf("") }
+    var isEmpty by remember { mutableStateOf(true) }
+
+    AlertDialog(
+        onDismissRequest = onCancel,
+        title = { Text(text = "Add symptom") },
+        text = {
+            Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
+                TextField(
+                    value = newSymptom,
+                    onValueChange = {
+                        if (it.endsWith("\n")) return@TextField
+                        newSymptom = it
+                        isEmpty = newSymptom.trim().isEmpty()
+                    }
+                )
+                if (isEmpty) {
+                    Text("Should not be empty", color = MaterialTheme.colorScheme.error)
+                }
+            }
+        },
+        confirmButton = {
+            Button(onClick = {
+                if (isEmpty) return@Button
+                onConfirm(newSymptom.trim())
+                newSymptom = ""
+            }) { Text("Confirm") }
+        },
+        dismissButton = {
+            TextButton(onClick = onCancel) { Text("Dismiss") }
+        }
+    )
 }
