fd1ec9fa3e1e23b87eadc204b856fa67cf6ad9fb
Laurin Rosrodowski <laurin.rosrodowski@stud.htwk-leipzig.de>
2025-07-03 12:24:47 +0200

    feat #2: (WIP) start work on tflite model integration in app

---
+++ app/src/main/java/org/htwk/pacing/backend/mlmodel/MLModel.kt
@@ -0,0 +1,86 @@
+package org.htwk.pacing.backend.mlmodel
+
+import android.content.Context
+import org.tensorflow.lite.Interpreter
+import org.tensorflow.lite.support.common.FileUtil
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import org.tensorflow.lite.DataType
+import java.nio.FloatBuffer
+import java.time.Instant
+import java.time.Duration
+import java.time.ZoneId
+import kotlin.math.*
+
+class MLModel(context: Context) {
+    private val interpreter: Interpreter
+
+    init {
+        // Load the TensorFlow Lite model
+        val model = FileUtil.loadMappedFile(context, "assets/hr_prediction_model/model.tflite");
+        interpreter = Interpreter(model)
+    }
+
+    private fun encodeTimeFeatures(instant: Instant, zone: ZoneId = ZoneId.systemDefault()): Pair<Pair<Double, Double>, Pair<Double, Double>> {
+        val zonedDateTime = instant.atZone(zone)
+
+        val secondsInDay = 24 * 60 * 60
+        val secondsInWeek = 7 * secondsInDay
+
+        val secondsSinceMidnight = zonedDateTime.toLocalTime().toSecondOfDay()
+        val dayOfWeekIndex = (zonedDateTime.dayOfWeek.value % 7) * secondsInDay + secondsSinceMidnight
+
+        val dayRatio = secondsSinceMidnight.toDouble() / secondsInDay
+        val weekRatio = dayOfWeekIndex.toDouble() / secondsInWeek
+
+        val daySin = sin(2 * PI * dayRatio)
+        val dayCos = cos(2 * PI * dayRatio)
+
+        val weekSin = sin(2 * PI * weekRatio)
+        val weekCos = cos(2 * PI * weekRatio)
+
+        return (daySin to dayCos) to (weekSin to weekCos)
+    }
+
+    fun predict(input: FloatArray, endTime: Instant): FloatArray {
+        val INPUT_DAYS = 2;
+        val TIMESTEPS = INPUT_DAYS * 24 * 6; // 10 minute timesteps
+        val FEATURE_COUNT = 5;
+        assert(input.size == TIMESTEPS); // for now
+
+        //TODO: how to deal with calculations happening in beetween the 10min steps
+
+        val inputBuffer = FloatBuffer.allocate(TIMESTEPS * FEATURE_COUNT);
+        for(i in 0..<TIMESTEPS) {
+            inputBuffer.put(input[i])
+
+            val timePoint = endTime.minus(Duration.ofMinutes((i * 10).toLong()));
+            val (day, week) = encodeTimeFeatures(timePoint);
+            val (day_sin, day_cos) = day;
+            val (week_sin, week_cos) = day;
+
+            inputBuffer.put(day_sin.toFloat())
+            inputBuffer.put(day_cos.toFloat())
+            inputBuffer.put(week_sin.toFloat())
+            inputBuffer.put(week_cos.toFloat())
+        }
+
+        inputBuffer.rewind()
+
+        val PRED_STEPS = 36
+        val outputBuffer = FloatBuffer.allocate(PRED_STEPS)
+        outputBuffer.rewind();
+
+        //run inference
+        interpreter.run(inputBuffer, outputBuffer);
+        outputBuffer.rewind();
+
+        //convert to return format (FloatArray instead of FloatBuffer)
+        val predictions = FloatArray(PRED_STEPS);
+        outputBuffer.get(predictions);
+
+        //val inputTensor = TensorBuffer.createFixedSize(intArrayOf(1, input.size), DataType.FLOAT32)
+        //val outputTensor = TensorBuffer.createFixedSize(intArrayOf(1, 10), DataType.FLOAT32)
+        //interpreter.run(inputTensor.buffer, outputTensor.buffer.rewind())
+        return predictions
+    }
+}
---
+++ app/src/main/java/org/htwk/pacing/ui/components/HeartRatePrediction.kt
@@ -0,0 +1,153 @@
+package org.htwk.pacing.ui.components
+
+import androidx.annotation.FloatRange
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.testTag
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import org.htwk.pacing.ui.lineTo
+import org.htwk.pacing.ui.math.Float2D
+import org.htwk.pacing.ui.moveTo
+import org.htwk.pacing.ui.relativeLineTo
+import org.htwk.pacing.ui.toPx
+import kotlin.time.Duration.Companion.hours
+
+/**
+ * Shows a graph of the last 12 hours of the users energy level
+ * and a prediction for the next 12 hours.
+ * The last value in the series is used as the current time.
+ */
+@Composable
+fun <C : Collection<Double>> HeartRatePredictionCard(
+    series: Series<C>,
+    seriesPredicted: Series<C>,
+    @FloatRange(from = 0.0, to = 1.0) minPrediction: Float,
+    @FloatRange(from = 0.0, to = 1.0) avgPrediction: Float,
+    @FloatRange(from = 0.0, to = 1.0) maxPrediction: Float,
+) {
+    CardWithTitle(title = "Heart Rate Prediction") {
+        if (series.x.isEmpty()) {
+            Text(
+                "Currently no data available!",
+                modifier = Modifier.testTag("EnergyPredictionErrorText")
+            )
+            return@CardWithTitle
+        }
+
+        val current = Instant.fromEpochMilliseconds(series.x.last().toLong())
+        val start = (current - 12.hours).toEpochMilliseconds().toDouble()
+        val end = (current + 12.hours).toEpochMilliseconds().toDouble()
+
+        val yConfig = AxisConfig(range = 0.0..1.0, steps = 0u)
+        val xConfig = AxisConfig(
+            range = start..end,
+            formatFunction = {
+                val localTime =
+                    Instant.fromEpochMilliseconds(it.toLong())
+                        .toLocalDateTime(TimeZone.currentSystemDefault())
+                "%02d:%02d".format(localTime.hour, localTime.minute)
+            }
+        )
+
+        Annotation(
+            series = series,
+            xConfig = xConfig,
+            yConfig = yConfig,
+        ) { _, yRange ->
+            Row(
+                modifier = Modifier.drawPrediction(
+                    series.y.lastOrNull()?.toFloat() ?: 0.5f,
+                    minPrediction,
+                    avgPrediction,
+                    maxPrediction
+                )
+            ) {
+                Graph(
+                    series = series,
+                    xRange = start..current.toEpochMilliseconds().toDouble(),
+                    yRange = yRange,
+                    modifier = Modifier.weight(1f),
+                )
+
+                val pc = PathConfig(Color(0xFFF96B6B), null, null, hasStroke = true, false)
+
+                Graph(
+                    series = seriesPredicted,
+                    xRange = start..current.toEpochMilliseconds().toDouble(),
+                    yRange = yRange,
+                    modifier = Modifier.weight(1f),
+                    pathConfig = pc
+                )
+            }
+        }
+    }
+}
+
+private fun Modifier.drawPrediction(
+    current: Float,
+    minPrediction: Float,
+    avgPrediction: Float,
+    maxPrediction: Float
+): Modifier = this.drawBehind {
+    val scope = this
+    val color = when {
+        avgPrediction < 0.4f -> Color(0xFFF96B6B)
+        avgPrediction < 0.6f -> Color(0xFFECC00A)
+        else -> Color(0xFF8FE02A)
+    }
+    val current = Float2D(0.5f, 1f - current)
+
+    val centerPath = Path().apply {
+        moveTo(scope, Float2D(0.5f, 0f))
+        lineTo(scope, Float2D(0.5f, 1f))
+    }
+    drawPath(
+        centerPath,
+        color = Color.Gray,
+        style = Stroke(
+            width = 3f,
+            pathEffect = PathEffect.dashPathEffect(floatArrayOf(30f, 20f))
+        ),
+    )
+
+    /*val predictionArea = Path().apply {
+        moveTo(scope, Float2D(1f, 1f - maxPrediction))
+        lineTo(scope, current)
+        lineTo(scope, Float2D(1f, 1f - minPrediction))
+        close()
+    }
+    drawPath(
+        predictionArea,
+        brush = Brush.linearGradient(
+            0f to color.copy(alpha = 0.5f),
+            1f to color.copy(alpha = 0.0f),
+            start = current.toPx(size),
+            end = Float2D(1f, 0.5f).toPx(size),
+        )
+    )
+
+    val predictionDirection = Path().apply {
+        moveTo(scope, current)
+        val scale = 0.3f
+        val direction = Float2D(0.5f, 0.5f - avgPrediction)
+            .normalize()
+            .scale(scale)
+        relativeLineTo(scope, direction)
+    }
+    drawPath(
+        predictionDirection,
+        color = color,
+        style = Stroke(5f)
+    )*/
+}
