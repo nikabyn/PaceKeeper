59f9d9e0aef9f150e6911f6d452ce243b7c9204d
Paul Köhler <paul.koehler@stud.htwk-leipzig.de>
2025-07-04 15:51:34 +0200

    fix: #22 change reference to sample in code

--- app/src/main/java/org/htwk/pacing/ui/components/ImportDataHealthConnect.kt
+++ app/src/main/java/org/htwk/pacing/ui/components/ImportDataHealthConnect.kt
@@ -6,10 +6,21 @@ import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
@@ -18,14 +29,16 @@ import androidx.health.connect.client.HealthConnectClient
 import androidx.health.connect.client.records.HeartRateRecord
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.io.*
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.ZonedDateTime
 
 /**
  * Testdaten für Import:
- * Beispieldaten unter res/raw/hr4.csv
+ * Beispieldaten unter samples/health_connect/hr4.csv
  */
 @Composable
 fun ImportDataHealthConnect() {
