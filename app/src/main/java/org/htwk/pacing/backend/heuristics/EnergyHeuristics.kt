11fbc7b241763ce4b6897fec94a0e12f1835dcba
Laurin Rosrodowski <laurin.rosrodowski@stud.htwk-leipzig.de>
2025-07-15 00:39:16 +0200

    feat #19: PR findings part 3: remove two left-over semicolons

--- app/src/main/java/org/htwk/pacing/backend/heuristics/EnergyHeuristics.kt
+++ app/src/main/java/org/htwk/pacing/backend/heuristics/EnergyHeuristics.kt
@@ -1,11 +1,10 @@
 package org.htwk.pacing.backend.heuristics
 
-import org.htwk.pacing.backend.database.HeartRateEntry
 import kotlin.math.max
 import kotlin.math.min
 
 //TODO: placeholder, to be replaced by scientific model trough ui feature ticket #21
-fun energyLevelFromHeartRate(heartRate: Double, /*liste von symptomen*/): Double {
-    val heartRateClipped = min(50.0, max(heartRate, 150.0) - 50.0);
-    return (100.0 - heartRateClipped) / 100.0;
+fun energyLevelFromHeartRate(heartRate: Double /*liste von symptomen*/): Double {
+    val heartRateClipped = min(50.0, max(heartRate, 150.0) - 50.0)
+    return (100.0 - heartRateClipped) / 100.0
 }
