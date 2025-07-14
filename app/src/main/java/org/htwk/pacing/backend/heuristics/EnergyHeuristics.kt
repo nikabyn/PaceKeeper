399441e6d9dfbf656142847483c0835fdef80b3e
Laurin Rosrodowski <laurin.rosrodowski@stud.htwk-leipzig.de>
2025-07-14 12:45:12 +0200

    feat #19: remove hr zone calculation to put in later pr

--- app/src/main/java/org/htwk/pacing/backend/heuristics/EnergyHeuristics.kt
+++ app/src/main/java/org/htwk/pacing/backend/heuristics/EnergyHeuristics.kt
@@ -17,33 +17,3 @@ fun energyLevelFromHeartRate(heartRate: Double, /*liste von symptomen*/): Double
     val heartRateClipped = min(50.0, max(heartRate, 150.0) - 50.0);
     return (100.0 - heartRateClipped) / 100.0;
 }
-
-data class HeartRateZone(
-    val bpmMin: Long,
-    val bpmMax: Long,
-)
-
-fun computeHeartRateZoneDistribution(heartRateData : List<HeartRateEntry>, heartRateZones : List<HeartRateZone>)
-: List<Pair<HeartRateZone, Double>> {
-    if (heartRateData.isEmpty()) {
-        return heartRateZones.map { it to 0.0 }
-    }
-
-    val zoneCounts = MutableList(heartRateZones.size) { 0L }
-
-    //TODO: weighted counting (similar to weighted average for 10-minute-intervals for model input)
-    for (entry in heartRateData) {
-        for ((index, zone) in heartRateZones.withIndex()) {
-            if (entry.bpm >= zone.bpmMin && entry.bpm <= zone.bpmMax) {
-                zoneCounts[index]++
-                break // Move to the next heart rate entry once a zone is found for it
-            }
-        }
-    }
-
-    val totalEntries = heartRateData.size.toDouble()
-    return heartRateZones.zip(zoneCounts).map { (zone, count) ->
-        val relativeAmount = if (totalEntries > 0) (count.toDouble() / totalEntries) else 0.0
-        zone to relativeAmount
-    }
-}
