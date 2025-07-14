596bb773134b26f9172d17d34021233fd28921e1
Paul Köhler <paul.koehler@stud.htwk-leipzig.de>
2025-07-14 18:48:52 +0200

    fix: #20 adjust imports

--- app/src/main/java/org/htwk/pacing/MainActivity.kt
+++ app/src/main/java/org/htwk/pacing/MainActivity.kt
@@ -7,8 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import org.htwk.pacing.backend.initNotificationSystem
 import org.htwk.pacing.backend.scheduleEnergyCheckWorker
 import org.htwk.pacing.ui.Main
-import org.htwk.pacing.ui.components.createNotificationChannel
-import org.htwk.pacing.ui.components.showNotification
+import org.htwk.pacing.backend.createNotificationChannel
+import org.htwk.pacing.backend.showNotification
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
