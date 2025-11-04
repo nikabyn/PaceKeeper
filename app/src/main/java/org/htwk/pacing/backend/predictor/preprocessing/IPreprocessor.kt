adcf67241ccd91cd21d4916ddc5e0c9465979b39
Laurin Rosrodowski <laurin.rosrodowski@stud.htwk-leipzig.de>
2025-11-04 20:00:36 +0100

    feat #37: PR findings #2

--- app/src/main/java/org/htwk/pacing/backend/predictor/preprocessing/IPreprocessor.kt
+++ app/src/main/java/org/htwk/pacing/backend/predictor/preprocessing/IPreprocessor.kt
@@ -1,19 +1,24 @@
 package org.htwk.pacing.backend.predictor.preprocessing
 
 import org.htwk.pacing.backend.predictor.Predictor
+import org.htwk.pacing.backend.predictor.preprocessing.IPreprocessor.DiscreteTimeSeriesResult.DiscretePID
 
 interface IPreprocessor {
-    //for results of continuous time series (like heart rate)
-    data class DiscretePID(
-        val proportional: DoubleArray,
-        val integral: DoubleArray,
-        val derivative: DoubleArray
-    )
 
-    //for results of integrable/summable time series (aggregations like steps)
-    data class DiscreteIntegral(
-        val integral: DoubleArray,
-    )
+    sealed interface DiscreteTimeSeriesResult {
+
+        //for results of continuous time series (like heart rate)
+        class DiscretePID(
+            val proportional: DoubleArray, // "P" part of PID
+            val integral: DoubleArray, // "I" part PID
+            val derivative: DoubleArray // "D" part of PID
+        ) : DiscreteTimeSeriesResult
+
+        //for results of integrable/summable time series (aggregations like steps)
+        data class DiscreteIntegral(
+            val integral: DoubleArray,
+        ) : DiscreteTimeSeriesResult
+    }
 
     //only used internally between preprocessor and model
     data class MultiTimeSeriesDiscrete(
