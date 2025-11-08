f9be5b848823757fd37416d3f9359b5b1998d4a0
Laurin Rosrodowski <laurin.rosrodowski@stud.htwk-leipzig.de>
2025-11-08 01:45:01 +0100

    feat #51: work on better extrapolation strategies (averaging so that we don't loose influence of some datapoints)

--- app/src/main/java/org/htwk/pacing/backend/predictor/model/LinearExtrapolator.kt
+++ app/src/main/java/org/htwk/pacing/backend/predictor/model/LinearExtrapolator.kt
@@ -4,11 +4,40 @@ import org.htwk.pacing.backend.predictor.Predictor
 
 object LinearExtrapolator {
 
-    private val lineIntervals = listOf(0, 3, 6, 12, 24, 36, 72);
+    private val stepsIntoFuture =
+        (Predictor.PREDICTION_WINDOW_DURATION.inWholeSeconds / Predictor.TIME_SERIES_STEP_DURATION.inWholeSeconds).toInt()
 
-    private val lines: List<Pair<Int, Int>> =
-        lineIntervals.map { Pair(it, 0) } +                 // (3,0), (6,0), ...
-                lineIntervals.zipWithNext { a, b -> Pair(b, a) }    // (6,3), (12,6), ..
+
+    data class ExtrapolationStrategy(
+        val firstAveragingRange: IntRange,
+
+        val secondAveragingRange: IntRange
+    )
+
+    enum class EXTRAPOLATION_STRATEGIES(val strategy: ExtrapolationStrategy) {
+        //enum for extrapolation from 3-hour average (6 hours in past) vs 3-hour average (9 hours in past)
+
+        NOW_VS_6_HOUR_TREND(
+            ExtrapolationStrategy(
+                IntRange(-(6 * 6), 0),
+                IntRange(0, 0)
+            )
+        ),
+        NOW_VS_12_HOUR_TREND(
+            ExtrapolationStrategy(
+                IntRange(-(6 * 6), 0),
+                IntRange(0, 0)
+            )
+        ),
+        PAST_6_HOUR_TREND(
+            ExtrapolationStrategy(
+                IntRange(-(6 * 12), -(-6 * 6)),
+                IntRange(0, 0)
+            )
+        ),
+
+
+    }
 
     //TODO: use fibonacci here for optimal logarithmic coverage
     //TODO: rethink shifted lines
@@ -18,9 +47,26 @@ object LinearExtrapolator {
         val extrapolations: List<Double>
     )
 
+    fun linearExtrapolate(x0: Int, y0: Double, x1: Int, y1: Double): Double {
+        val slope = (y1 - y0) / (x1 - x0).toDouble()
+        return y1 + slope * (x1 + stepsIntoFuture)
+    }
+
+    //TODO: value now vs average of window
+
     fun multipleExtrapolate(timeSeries: DoubleArray): MultiExtrapolationResult {
-        val stepsToExtrapolate =
-            (Predictor.PREDICTION_WINDOW_DURATION.inWholeSeconds / Predictor.TIME_SERIES_STEP_DURATION.inWholeSeconds).toInt()
+        val latestValue = timeSeries.last();
+
+        //now vs 6-hour average
+        val averageLast6Hours = timeSeries.slice(timeSeries.size - 36..timeSeries.size).average()
+        val extrapolation = linearExtrapolate(-18, averageLast6Hours, 0, latestValue)
+
+        val averageLast12Hours = timeSeries.slice(timeSeries.size - 72..timeSeries.size).average()
+        val extrapolation2 = linearExtrapolate(-36, averageLast12Hours, 0, latestValue)
+
+        //short-term trend
+        val extrapolation3 =
+            linearExtrapolate(-3, timeSeries[timeSeries.size - 1 - 3], 0, latestValue)
 
         return MultiExtrapolationResult(extrapolations = lines.map { line ->
             val startIdx = timeSeries.size - 1 - line.first   //first discrete timepoint of line
---
+++ app/src/test/java/org/htwk/pacing/backend/predictor/model/LinearExtrapolatorTest.kt
@@ -0,0 +1,13 @@
+package org.htwk.pacing.backend.predictor.model
+
+import org.junit.Test
+
+class LinearExtrapolatorTest {
+
+    @Test
+    fun `Extrapolation with a positive  upward trend`() {
+        // Given a timeSeries with a clear and consistent positive slope,
+        // verify that the extrapolated values are positive and greater than the last element of the time series.
+        // TODO implement test
+    }
+}
