87a373c3afb842cddff4ec9be9362c7d135044bf
Laurin Rosrodowski <laurin.rosrodowski@stud.htwk-leipzig.de>
2025-11-08 02:21:12 +0100

    feat #51: implement averaging strategies

--- app/src/main/java/org/htwk/pacing/backend/predictor/model/LinearExtrapolator.kt
+++ app/src/main/java/org/htwk/pacing/backend/predictor/model/LinearExtrapolator.kt
@@ -8,76 +8,142 @@ object LinearExtrapolator {
         (Predictor.PREDICTION_WINDOW_DURATION.inWholeSeconds / Predictor.TIME_SERIES_STEP_DURATION.inWholeSeconds).toInt()
 
 
-    data class ExtrapolationStrategy(
+    class ExtrapolationStrategy(
         val firstAveragingRange: IntRange,
-
         val secondAveragingRange: IntRange
-    )
+    ) {
+        private fun linearExtrapolate(x0: Int, y0: Double, x1: Int, y1: Double): Double {
+            val slope = (y1 - y0) / (x1 - x0).toDouble()
+            return y1 + slope * (x1 + stepsIntoFuture)
+        }
 
-    enum class EXTRAPOLATION_STRATEGIES(val strategy: ExtrapolationStrategy) {
-        //enum for extrapolation from 3-hour average (6 hours in past) vs 3-hour average (9 hours in past)
+        fun runOnTimeSeries(timeSeries: DoubleArray): Double {
+            val latestIndex = timeSeries.size - 1
 
-        NOW_VS_6_HOUR_TREND(
-            ExtrapolationStrategy(
-                IntRange(-(6 * 6), 0),
-                IntRange(0, 0)
-            )
-        ),
-        NOW_VS_12_HOUR_TREND(
-            ExtrapolationStrategy(
-                IntRange(-(6 * 6), 0),
-                IntRange(0, 0)
-            )
-        ),
-        PAST_6_HOUR_TREND(
-            ExtrapolationStrategy(
-                IntRange(-(6 * 12), -(-6 * 6)),
-                IntRange(0, 0)
-            )
-        ),
+            val firstPointRange =
+                (latestIndex - firstAveragingRange.first)..(latestIndex - firstAveragingRange.last)
+            val y0 = timeSeries.slice(firstPointRange).average()
+            val x0 = -(firstAveragingRange.first + firstAveragingRange.last) / 2
 
+            val secondPointRange =
+                (latestIndex - secondAveragingRange.first)..(latestIndex - secondAveragingRange.last)
+            val y1 = timeSeries.slice(secondPointRange).average()
+            val x1 = -(secondAveragingRange.first + secondAveragingRange.last) / 2
 
+            return linearExtrapolate(x0, y0, x1, y1)
+        }
     }
 
     //TODO: use fibonacci here for optimal logarithmic coverage
     //TODO: rethink shifted lines
     //TODO: use average windows per line, that correspond to trend window we're looking at
+    enum class EXTRAPOLATION_STRATEGY(val strategy: ExtrapolationStrategy) {
+        NOW_VS_30_MINUTES_AGO(
+            ExtrapolationStrategy(
+                IntRange(-3, -3),
+                IntRange(0, 0)
+            )
+        ),
+        NOW_VS_60_MINUTES_AGO(
+            ExtrapolationStrategy(
+                IntRange(-6, -6),
+                IntRange(0, 0)
+            )
+        ),
+        NOW_VS_90_MINUTES_AGO(
+            ExtrapolationStrategy(
+                IntRange(-9, -9),
+                IntRange(0, 0)
+            )
+        ),
+        NOW_VS_120_MINUTES_AGO(
+            ExtrapolationStrategy(
+                IntRange(-12, -12),
+                IntRange(0, 0)
+            )
+        ),
 
-    data class MultiExtrapolationResult(
-        val extrapolations: List<Double>
-    )
-
-    fun linearExtrapolate(x0: Int, y0: Double, x1: Int, y1: Double): Double {
-        val slope = (y1 - y0) / (x1 - x0).toDouble()
-        return y1 + slope * (x1 + stepsIntoFuture)
+        HOURLY_AVG_NOW_VS_1_HOUR_AGO(
+            ExtrapolationStrategy(
+                IntRange((6 * 2), 6),
+                IntRange(6, 0)
+            )
+        ),
+        HOURLY_AVG_NOW_VS_2_HOURS_AGO(
+            ExtrapolationStrategy(
+                IntRange((6 * 3), (6 * 2)),
+                IntRange(6, 0)
+            )
+        ),
+        HOURLY_AVG_NOW_VS_3_HOURS_AGO(
+            ExtrapolationStrategy(
+                IntRange((6 * 4), (6 * 3)),
+                IntRange(6, 0)
+            )
+        ),
+        NOW_VS_1_HOUR_TREND(
+            ExtrapolationStrategy(
+                IntRange((6 * 1), 0),
+                IntRange(0, 0)
+            )
+        ),
+        NOW_VS_3_HOUR_TREND(
+            ExtrapolationStrategy(
+                IntRange((6 * 3), 0),
+                IntRange(0, 0)
+            )
+        ),
+        NOW_VS_6_HOUR_TREND(
+            ExtrapolationStrategy(
+                IntRange((6 * 6), 0),
+                IntRange(0, 0)
+            )
+        ),
+        NOW_VS_12_HOUR_TREND(
+            ExtrapolationStrategy(
+                IntRange((6 * 12), 0),
+                IntRange(0, 0)
+            )
+        ),
+        PAST_3_HOUR_TREND(
+            ExtrapolationStrategy(
+                IntRange((6 * 6), (6 * 3)),
+                IntRange((6 * 3), 0)
+            )
+        ),
+        PAST_6_HOUR_TREND(
+            ExtrapolationStrategy(
+                IntRange((6 * 12), (6 * 6)),
+                IntRange((6 * 6), 0)
+            )
+        ),
+        PAST_12_HOUR_TREND(
+            ExtrapolationStrategy(
+                IntRange((6 * 24), (6 * 12)),
+                IntRange((6 * 12), 0)
+            )
+        ),
+        LAST_HOUR_AVERAGE_VS_1_DAY_AGO(
+            ExtrapolationStrategy(
+                IntRange((6 * 25), (6 * 24)),
+                IntRange((6 * 1), 0)
+            )
+        ),
+        YESTERDAY_VS_TODAY(
+            ExtrapolationStrategy(
+                IntRange((6 * 48), (6 * 24)),
+                IntRange((6 * 24), 0)
+            )
+        )
     }
 
-    //TODO: value now vs average of window
+    data class MultiExtrapolationResult(
+        val extrapolations: Map<EXTRAPOLATION_STRATEGY, Double>
+    )
 
     fun multipleExtrapolate(timeSeries: DoubleArray): MultiExtrapolationResult {
-        val latestValue = timeSeries.last();
-
-        //now vs 6-hour average
-        val averageLast6Hours = timeSeries.slice(timeSeries.size - 36..timeSeries.size).average()
-        val extrapolation = linearExtrapolate(-18, averageLast6Hours, 0, latestValue)
-
-        val averageLast12Hours = timeSeries.slice(timeSeries.size - 72..timeSeries.size).average()
-        val extrapolation2 = linearExtrapolate(-36, averageLast12Hours, 0, latestValue)
-
-        //short-term trend
-        val extrapolation3 =
-            linearExtrapolate(-3, timeSeries[timeSeries.size - 1 - 3], 0, latestValue)
-
-        return MultiExtrapolationResult(extrapolations = lines.map { line ->
-            val startIdx = timeSeries.size - 1 - line.first   //first discrete timepoint of line
-            val endIdx = timeSeries.size - 1 - line.second    //second discrete timepoint of line
-
-            val startY = timeSeries[startIdx] //value at first point
-            val endY = timeSeries[endIdx] //value at second point
-
-            val slope = (endY - startY) / (endIdx - startIdx) //slope between the points
-            val result = endY + slope * (endY + stepsToExtrapolate) //linear trend extrapolation
-            result //return the result in this lambda expression
+        return MultiExtrapolationResult(extrapolations = EXTRAPOLATION_STRATEGY.entries.associateWith {
+            it.strategy.runOnTimeSeries(timeSeries)
         })
     }
 }
