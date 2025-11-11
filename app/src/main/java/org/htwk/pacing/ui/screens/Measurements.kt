d6ff44b05676c25d48b981b5e4d6f445d81df532
Julie Renner <julie.renner@stud.htwk-leipzig.de>
2025-11-11 18:43:30 +0100

    feat: #48.2 edit 12h HRGraphCard
    # Conflicts:
    #	app/src/main/java/org/htwk/pacing/ui/screens/Measurements.kt

--- app/src/main/java/org/htwk/pacing/ui/screens/Measurements.kt
+++ app/src/main/java/org/htwk/pacing/ui/screens/Measurements.kt
@@ -149,6 +149,29 @@ fun MeasurementsScreen(
                 ),
                 pathConfig = pathConfig,
             )
+            HRGraphCard(
+                title = stringResource(R.string.heart_rate_last_12_hours),
+                series = heartRate,
+                xConfig = AxisConfig(
+                    formatFunction = {
+                        val localTime = Instant.fromEpochMilliseconds(it.toLong())
+                            .toLocalDateTime(TimeZone.currentSystemDefault())
+                        "%02d:%02d:%02d".format(
+                            localTime.hour,
+                            localTime.minute,
+                            localTime.second
+                        )
+                    },
+                    range = time12hoursAgo.toEpochMilliseconds().toDouble()
+                            ..timeNow.toEpochMilliseconds().toDouble(),
+                    steps = 2u,
+                ),
+                yConfig = AxisConfig(
+                    range = 40.0..160.0
+                ),
+                pathConfig = pathConfig,
+                zonesResult = HeartRateZones.calculateZones(input)
+            )
 
             HistogramCard(
                 title = stringResource(R.string.heart_rate_histogram_last_24_hours),
