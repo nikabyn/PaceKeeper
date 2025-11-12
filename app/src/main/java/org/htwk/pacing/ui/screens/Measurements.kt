a0a7f37a5304130f8f015efb36b00124a10f08e0
Julie Renner <julie.renner@stud.htwk-leipzig.de>
2025-11-12 19:53:43 +0100

    feat: #48 clean up

--- app/src/main/java/org/htwk/pacing/ui/screens/Measurements.kt
+++ app/src/main/java/org/htwk/pacing/ui/screens/Measurements.kt
@@ -70,9 +70,7 @@ fun MeasurementsScreen(
     val predictedHeartRate by viewModel.predictedHeartRate.collectAsState()
     val predictedEnergyLevel by viewModel.predictedEnergyLevel.collectAsState()
     val heartRateZonesResult by viewModel.heartRateZonesResult.collectAsState()
-    // provisorische Werte, werden aus der
-    // val input = HeartRateZones.HeartRateInput(, HeartRateZones.Sex.FEMALE, 50)
-    //
+
     var timeNow by remember { mutableStateOf(Clock.System.now()) }
     var time12hoursAgo by remember { mutableStateOf(timeNow - 12.hours) }
     var time7daysAgo by remember { mutableStateOf(timeNow - 7.days) }
