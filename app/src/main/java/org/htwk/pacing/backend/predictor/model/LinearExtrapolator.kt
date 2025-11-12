5b17095f664d6fa156a20f7ac88d76b82f304f87
Laurin Rosrodowski <laurin.rosrodowski@stud.htwk-leipzig.de>
2025-11-12 12:59:47 +0100

    feat #51: PR finding: implement better abstraction through SamplingDescriptor interface (average vs. single-point)

--- app/src/main/java/org/htwk/pacing/backend/predictor/model/LinearExtrapolator.kt
+++ app/src/main/java/org/htwk/pacing/backend/predictor/model/LinearExtrapolator.kt
@@ -12,31 +12,49 @@ object LinearExtrapolator {
         val resultPoint: Pair<Double, Double>
     )
 
+    private fun linearExtrapolate(x0: Double, y0: Double, x1: Double, y1: Double): Double {
+        val slope = (y1 - y0) / -(x1 - x0)
+        return y1 + slope * (x1 + stepsIntoFuture)
+    }
+
+    sealed interface SamplingDescriptor {
+        fun getSampleResultY(timeSeries: DoubleArray): Double
+
+        fun getSamplePositionX(): Double
+
+        data class SinglePoint(val index: Int) : SamplingDescriptor {
+            override fun getSamplePositionX(): Double {
+                return index.toDouble()
+            }
+
+            override fun getSampleResultY(timeSeries: DoubleArray): Double {
+                val lastIndex = timeSeries.size - 1
+                return timeSeries[lastIndex - index]
+            }
+        }
+
+        data class Average(val indexRange: IntRange) : SamplingDescriptor {
+            override fun getSamplePositionX(): Double {
+                return (indexRange.first + indexRange.last) / 2.0
+            }
+
+            override fun getSampleResultY(timeSeries: DoubleArray): Double {
+                val lastIndex = timeSeries.size - 1
+                return timeSeries.slice((lastIndex - indexRange.first)..(lastIndex - indexRange.last))
+                    .average()
+            }
+        }
+    }
+
     data class ExtrapolationStrategy(
-        val firstAveragingRange: IntRange,
-        val secondAveragingRange: IntRange,
+        val samplingDescriptors: Pair<SamplingDescriptor, SamplingDescriptor>
     ) {
-        private fun linearExtrapolate(x0: Double, y0: Double, x1: Double, y1: Double): Double {
-            val slope = (y1 - y0) / -(x1 - x0)
-            return y1 + slope * (x1 + stepsIntoFuture)
-        }
-
-        private fun IntRange.average(): Double {
-            return (this.first + this.last) / 2.0
-        }
-
-        private fun IntRange.getAverageValueFrom(timeSeries: DoubleArray): Double {
-            val lastIndex = timeSeries.size - 1
-            return timeSeries.slice((lastIndex - first)..(lastIndex - last)).average()
-        }
-
         fun runOnTimeSeries(timeSeries: DoubleArray): ExtrapolationLine {
-            val (x0, y0) = firstAveragingRange.average() to firstAveragingRange.getAverageValueFrom(
-                timeSeries
-            )
-            val (x1, y1) = secondAveragingRange.average() to secondAveragingRange.getAverageValueFrom(
-                timeSeries
-            )
+            val x0 = samplingDescriptors.first.getSamplePositionX()
+            val y0 = samplingDescriptors.first.getSampleResultY(timeSeries)
+
+            val x1 = samplingDescriptors.second.getSamplePositionX()
+            val y1 = samplingDescriptors.second.getSampleResultY(timeSeries)
 
             val result = linearExtrapolate(x0 = x0, y0 = y0, x1 = x1, y1 = y1)
 
@@ -64,99 +82,131 @@ object LinearExtrapolator {
     enum class EXTRAPOLATION_STRATEGY(val strategy: ExtrapolationStrategy) {
         NOW_VS_30_MINUTES_AGO(
             ExtrapolationStrategy(
-                IntRange(3, 3),
-                IntRange(0, 0)
+                Pair(
+                    SamplingDescriptor.SinglePoint(3),
+                    SamplingDescriptor.SinglePoint(0)
+                )
             )
         ),
         NOW_VS_60_MINUTES_AGO(
             ExtrapolationStrategy(
-                IntRange(6, 6),
-                IntRange(0, 0)
+                Pair(
+                    SamplingDescriptor.SinglePoint(6),
+                    SamplingDescriptor.SinglePoint(0)
+                )
             )
         ),
         NOW_VS_90_MINUTES_AGO(
             ExtrapolationStrategy(
-                IntRange(9, 9),
-                IntRange(0, 0)
+                Pair(
+                    SamplingDescriptor.SinglePoint(9),
+                    SamplingDescriptor.SinglePoint(0)
+                )
             )
         ),
         NOW_VS_120_MINUTES_AGO(
             ExtrapolationStrategy(
-                IntRange(12, 12),
-                IntRange(0, 0)
+                Pair(
+                    SamplingDescriptor.SinglePoint(12),
+                    SamplingDescriptor.SinglePoint(0)
+                )
             )
         ),
 
         HOURLY_AVG_NOW_VS_1_HOUR_AGO(
             ExtrapolationStrategy(
-                IntRange((6 * 2), 6),
-                IntRange(6, 0)
+                Pair(
+                    SamplingDescriptor.Average(IntRange(12, 6)),
+                    SamplingDescriptor.Average(IntRange(6, 0))
+                )
             )
         ),
         HOURLY_AVG_NOW_VS_2_HOURS_AGO(
             ExtrapolationStrategy(
-                IntRange((6 * 3), (6 * 2)),
-                IntRange(6, 0)
+                Pair(
+                    SamplingDescriptor.Average(IntRange(18, 12)),
+                    SamplingDescriptor.Average(IntRange(6, 0))
+                )
             )
         ),
         HOURLY_AVG_NOW_VS_3_HOURS_AGO(
             ExtrapolationStrategy(
-                IntRange((6 * 4), (6 * 3)),
-                IntRange(6, 0)
+                Pair(
+                    SamplingDescriptor.Average(IntRange(24, 18)),
+                    SamplingDescriptor.Average(IntRange(6, 0))
+                )
             )
         ),
         NOW_VS_1_HOUR_TREND(
             ExtrapolationStrategy(
-                IntRange((6 * 1), 0),
-                IntRange(0, 0)
+                Pair(
+                    SamplingDescriptor.Average(IntRange(6, 0)),
+                    SamplingDescriptor.SinglePoint(0)
+                )
             )
         ),
         NOW_VS_3_HOUR_TREND(
             ExtrapolationStrategy(
-                IntRange((6 * 3), 0),
-                IntRange(0, 0)
+                Pair(
+                    SamplingDescriptor.Average(IntRange(18, 0)),
+                    SamplingDescriptor.SinglePoint(0)
+                )
             )
         ),
         NOW_VS_6_HOUR_TREND(
             ExtrapolationStrategy(
-                IntRange((6 * 6), 0),
-                IntRange(0, 0)
+                Pair(
+                    SamplingDescriptor.Average(IntRange(36, 0)),
+                    SamplingDescriptor.SinglePoint(0)
+                )
             )
         ),
         NOW_VS_12_HOUR_TREND(
             ExtrapolationStrategy(
-                IntRange((6 * 12), 0),
-                IntRange(0, 0)
+                Pair(
+                    SamplingDescriptor.Average(IntRange(72, 0)),
+                    SamplingDescriptor.SinglePoint(0)
+                )
             )
         ),
         PAST_3_HOUR_TREND(
             ExtrapolationStrategy(
-                IntRange((6 * 6), (6 * 3)),
-                IntRange((6 * 3), 0)
+                Pair(
+                    SamplingDescriptor.Average(IntRange(36, 18)),
+                    SamplingDescriptor.Average(IntRange(18, 0))
+                )
             )
         ),
         PAST_6_HOUR_TREND(
             ExtrapolationStrategy(
-                IntRange((6 * 12), (6 * 6)),
-                IntRange((6 * 6), 0)
+                Pair(
+                    SamplingDescriptor.Average(IntRange(72, 36)),
+                    SamplingDescriptor.Average(IntRange(36, 0))
+                )
             )
         ),
         PAST_12_HOUR_TREND(
             ExtrapolationStrategy(
-                IntRange((6 * 24), (6 * 12)),
-                IntRange((6 * 12), 0)
+                Pair(
+                    SamplingDescriptor.Average(IntRange(144, 72)),
+                    SamplingDescriptor.Average(IntRange(72, 0))
+                )
             )
         ),
         LAST_HOUR_AVERAGE_VS_1_DAY_AGO(
             ExtrapolationStrategy(
-                IntRange((6 * 25), (6 * 24)),
-                IntRange((6 * 1), 0)
+                Pair(
+                    SamplingDescriptor.Average(IntRange(150, 144)),
+                    SamplingDescriptor.Average(IntRange(6, 0))
+                )
             )
         ),
         YESTERDAY_VS_TODAY(
             ExtrapolationStrategy(
-                IntRange((6 * 48 - 1), (6 * 24 - 1)),
-                IntRange((6 * 24), 0)
+                Pair(
+                    SamplingDescriptor.Average(IntRange((6 * 48 - 1), (6 * 24 - 1))),
+                    SamplingDescriptor.Average(IntRange((6 * 24), 0))
+                )
             )
         )
     }
