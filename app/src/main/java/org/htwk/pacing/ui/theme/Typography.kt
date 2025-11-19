638c2d0f889ae13721fc86920dc0f9b437e73258
Nika Sommer <nika.sommer@stud.htwk-leipzig.de>
2025-11-19 07:04:39 +0100

    fix: #63 fix variable font weight

--- app/src/main/java/org/htwk/pacing/ui/theme/Typography.kt
+++ app/src/main/java/org/htwk/pacing/ui/theme/Typography.kt
@@ -4,28 +4,40 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontVariation
+import androidx.compose.ui.text.font.FontWeight
 import org.htwk.pacing.R
 
-/**
- * Variable fonts are supported by all of our
- */
+
+private val defaultTypography = Typography()
+
 @OptIn(ExperimentalTextApi::class)
-private val fontFamily = FontFamily(Font(resId = R.font.google_sans_flex))
+private val fontFamily = FontFamily(
+    arrayOf(300, 400, 500, 600, 700, 800).map { weight ->
+        Font(
+            resId = R.font.google_sans_flex,
+            weight = FontWeight(weight),
+            variationSettings = FontVariation.Settings(
+                FontVariation.Setting("wght", weight.toFloat()),
+            )
+        )
+    }
+)
 
 val Typography = Typography(
-    displayLarge = Typography().displayLarge.copy(fontFamily = fontFamily),
-    displayMedium = Typography().displayMedium.copy(fontFamily = fontFamily),
-    displaySmall = Typography().displaySmall.copy(fontFamily = fontFamily),
-    headlineLarge = Typography().headlineLarge.copy(fontFamily = fontFamily),
-    headlineMedium = Typography().headlineMedium.copy(fontFamily = fontFamily),
-    headlineSmall = Typography().headlineSmall.copy(fontFamily = fontFamily),
-    titleLarge = Typography().titleLarge.copy(fontFamily = fontFamily),
-    titleMedium = Typography().titleMedium.copy(fontFamily = fontFamily),
-    titleSmall = Typography().titleSmall.copy(fontFamily = fontFamily),
-    bodyLarge = Typography().bodyLarge.copy(fontFamily = fontFamily),
-    bodyMedium = Typography().bodyMedium.copy(fontFamily = fontFamily),
-    bodySmall = Typography().bodySmall.copy(fontFamily = fontFamily),
-    labelLarge = Typography().labelLarge.copy(fontFamily = fontFamily),
-    labelMedium = Typography().labelMedium.copy(fontFamily = fontFamily),
-    labelSmall = Typography().labelSmall.copy(fontFamily = fontFamily),
+    displayLarge = defaultTypography.displayLarge.copy(fontFamily = fontFamily),
+    displayMedium = defaultTypography.displayMedium.copy(fontFamily = fontFamily),
+    displaySmall = defaultTypography.displaySmall.copy(fontFamily = fontFamily),
+    headlineLarge = defaultTypography.headlineLarge.copy(fontFamily = fontFamily),
+    headlineMedium = defaultTypography.headlineMedium.copy(fontFamily = fontFamily),
+    headlineSmall = defaultTypography.headlineSmall.copy(fontFamily = fontFamily),
+    titleLarge = defaultTypography.titleLarge.copy(fontFamily = fontFamily),
+    titleMedium = defaultTypography.titleMedium.copy(fontFamily = fontFamily),
+    titleSmall = defaultTypography.titleSmall.copy(fontFamily = fontFamily),
+    bodyLarge = defaultTypography.bodyLarge.copy(fontFamily = fontFamily),
+    bodyMedium = defaultTypography.bodyMedium.copy(fontFamily = fontFamily),
+    bodySmall = defaultTypography.bodySmall.copy(fontFamily = fontFamily),
+    labelLarge = defaultTypography.labelLarge.copy(fontFamily = fontFamily),
+    labelMedium = defaultTypography.labelMedium.copy(fontFamily = fontFamily),
+    labelSmall = defaultTypography.labelSmall.copy(fontFamily = fontFamily),
 )
