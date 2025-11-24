9f0cc78a2174b987345fb18cd0ca253929ac36dd
Nika Sommer <nika.sommer@stud.htwk-leipzig.de>
2025-11-24 14:37:52 +0100

    feat: #64 adjust ui to follow styleguide

--- app/src/main/java/org/htwk/pacing/ui/screens/Measurements.kt
+++ app/src/main/java/org/htwk/pacing/ui/screens/Measurements.kt
@@ -1,7 +1,6 @@
 package org.htwk.pacing.ui.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,11 +17,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.StrokeJoin
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.lerp
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
@@ -50,10 +44,8 @@ import org.htwk.pacing.ui.components.GraphCard
 import org.htwk.pacing.ui.components.HeartRateGraphCard
 import org.htwk.pacing.ui.components.HeartRatePredictionCard
 import org.htwk.pacing.ui.components.HistogramCard
-import org.htwk.pacing.ui.components.PathConfig
 import org.htwk.pacing.ui.components.Series
-import org.htwk.pacing.ui.components.withFill
-import org.htwk.pacing.ui.components.withStroke
+import org.htwk.pacing.ui.theme.Spacing
 import org.koin.androidx.compose.koinViewModel
 import kotlin.time.Duration.Companion.days
 import kotlin.time.Duration.Companion.hours
@@ -83,22 +75,14 @@ fun MeasurementsScreen(
         }
     }
 
-    val pathConfig = PathConfig
-        .withStroke(
-            color = if (isSystemInDarkTheme()) lerp(Color.Red, Color.White, 0.5f) else Color.Red,
-            style = Stroke(width = 1.5f, cap = StrokeCap.Round, join = StrokeJoin.Round),
-        ).withFill(
-            color = Color.hsv(0.0f, 0.5f, 1.0f, 0.3f)
-        )
-
     Box(
         modifier = modifier
             .verticalScroll(rememberScrollState())
             .background(MaterialTheme.colorScheme.background)
     ) {
         Column(
-            verticalArrangement = Arrangement.spacedBy(30.dp),
-            modifier = Modifier.padding(all = 40.dp)
+            verticalArrangement = Arrangement.spacedBy(Spacing.largeIncreased),
+            modifier = Modifier.padding(horizontal = Spacing.large, vertical = Spacing.extraLarge)
         ) {
             fun formatTime(value: Double): String {
                 val localTime =
