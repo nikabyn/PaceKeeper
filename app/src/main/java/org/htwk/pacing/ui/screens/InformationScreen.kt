f28d1ca040ef39871ec9f3a334379cb17ed05811
Nika Sommer <nika.sommer@stud.htwk-leipzig.de>
2025-12-12 16:27:37 +0100

    feat: #68 add welcome screen button on settings

--- app/src/main/java/org/htwk/pacing/ui/screens/InformationScreen.kt
+++ app/src/main/java/org/htwk/pacing/ui/screens/InformationScreen.kt
@@ -2,11 +2,8 @@ package org.htwk.pacing.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,6 +16,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import org.htwk.pacing.R
+import org.htwk.pacing.ui.Route
 import org.htwk.pacing.ui.components.Button
 import org.htwk.pacing.ui.components.SettingsSubScreen
 import org.htwk.pacing.ui.theme.PrimaryButtonStyle
@@ -41,30 +39,30 @@ fun InformationScreen(
             verticalArrangement = Arrangement.spacedBy(Spacing.large)
         ) {
             Button(
+                onClick = { navController.navigate(Route.WELCOME) },
+                style = PrimaryButtonStyle,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(stringResource(R.string.welcome_screen))
+            }
+            Button(
                 onClick = { showPrivacyPolicyDialog = true },
                 style = PrimaryButtonStyle,
                 modifier = Modifier.fillMaxWidth()
             ) {
-                Text(
-                    (stringResource(R.string.privacy_policy)),
-                    style = MaterialTheme.typography.bodyMedium
-                )
+                Text(stringResource(R.string.privacy_policy))
             }
-            if (showPrivacyPolicyDialog) {
-                PrivacyPolicyDialog(
-                    onDismiss = { showPrivacyPolicyDialog = false }
-                )
-            }
-            Spacer(modifier = Modifier.height(Spacing.large))
-
             Button(
                 onClick = { showLicenceDialog = true },
                 style = PrimaryButtonStyle,
                 modifier = Modifier.fillMaxWidth()
             ) {
-                Text(
-                    (stringResource(R.string.view_licence)),
-                    style = MaterialTheme.typography.bodyMedium
+                Text(stringResource(R.string.view_licence))
+            }
+
+            if (showPrivacyPolicyDialog) {
+                PrivacyPolicyDialog(
+                    onDismiss = { showPrivacyPolicyDialog = false }
                 )
             }
             if (showLicenceDialog) {
