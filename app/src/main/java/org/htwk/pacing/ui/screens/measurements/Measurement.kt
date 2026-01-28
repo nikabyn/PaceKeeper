533040519834bd6e3a107b121608a8b88549000c
Nika Sommer <nika.sommer@stud.htwk-leipzig.de>
2026-01-28 17:32:20 +0100

    feat: #88 add missing i18n

--- app/src/main/java/org/htwk/pacing/ui/screens/measurements/Measurement.kt
+++ app/src/main/java/org/htwk/pacing/ui/screens/measurements/Measurement.kt
@@ -1,11 +1,13 @@
 package org.htwk.pacing.ui.screens.measurements
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.atStartOfDayIn
 import kotlinx.datetime.toLocalDateTime
+import org.htwk.pacing.R
 import org.htwk.pacing.backend.database.DistanceEntry
 import org.htwk.pacing.backend.database.ElevationGainedEntry
 import org.htwk.pacing.backend.database.HeartRateEntry
@@ -48,19 +50,19 @@ enum class Measurement {
     SkinTemperature,
     Symptoms;
 
-    // TODO i18n
+    @Composable
     fun title() = when (this) {
-        Steps -> "Steps"
-        Distance -> "Distance"
-        ElevationGained -> "Elevation gained"
-        Speed -> "Speed"
-        HeartRate -> "Heart rate"
-        MenstruationPeriod -> "Menstruation"
-        OxygenSaturation -> "Oxygen saturation"
-        Sleep -> "Sleep"
-        HeartRateVariabilityRmssd -> "Heart rate variability"
-        SkinTemperature -> "Skin temperature"
-        Symptoms -> "Symptoms"
+        Steps -> stringResource(R.string.steps)
+        Distance -> stringResource(R.string.distance)
+        ElevationGained -> stringResource(R.string.elevation_gained)
+        Speed -> stringResource(R.string.speed)
+        HeartRate -> stringResource(R.string.heart_rate)
+        MenstruationPeriod -> stringResource(R.string.menstruation)
+        OxygenSaturation -> stringResource(R.string.oxygen_saturation)
+        Sleep -> stringResource(R.string.sleep)
+        HeartRateVariabilityRmssd -> stringResource(R.string.heart_rate_variability)
+        SkinTemperature -> stringResource(R.string.skin_temperature_variation)
+        Symptoms -> stringResource(R.string.symptoms)
     }
 
     fun dao(db: PacingDatabase) = when (this) {
@@ -85,9 +87,25 @@ enum class Measurement {
             ElevationGained -> (entry as ElevationGainedEntry).length.inMeters()
             Speed -> (entry as SpeedEntry).velocity.inKilometersPerHour()
             HeartRate -> (entry as HeartRateEntry).bpm.toDouble()
-            MenstruationPeriod -> 0.0 // TODO
+            MenstruationPeriod -> 1.0
             OxygenSaturation -> (entry as OxygenSaturationEntry).percentage.toDouble()
-            Sleep -> 0.0 // TODO
+
+            Sleep -> (entry as SleepSessionEntry).let {
+                when (it.stage) {
+                    SleepStage.Unknown,
+                    SleepStage.AwakeInBed,
+                    SleepStage.Awake,
+                    SleepStage.OutOfBed -> 0.0
+
+                    SleepStage.REM -> 1.0
+
+                    SleepStage.Sleeping,
+                    SleepStage.Light -> 2.0
+
+                    SleepStage.Deep -> 3.0
+                }
+            }
+
             HeartRateVariabilityRmssd -> (entry as HeartRateVariabilityEntry).variability
             SkinTemperature -> (entry as SkinTemperatureEntry).temperature.inCelsius()
             Symptoms -> (entry as ManualSymptomEntry).feeling.feeling.level.toDouble()
@@ -104,7 +122,7 @@ enum class Measurement {
             HeartRate -> 0.0..(yMax ?: 150.0)
             MenstruationPeriod -> 0.0..24.0
             OxygenSaturation -> 0.0..1.0
-            Sleep -> 0.0..24.0
+            Sleep -> 0.0..3.0
             HeartRateVariabilityRmssd -> 0.0..1.0
             SkinTemperature -> -20.0..60.0
             Symptoms -> 0.0..3.0
@@ -113,36 +131,33 @@ enum class Measurement {
 }
 
 data class MeasurementStatistic(val measurement: Measurement, val value: String?) {
-    // TODO i18n
-    @Composable
     fun unit(): String? = when (this.measurement) {
         Steps -> null
         Distance -> "km"
         ElevationGained -> "m"
         Speed -> "km/h"
         HeartRate -> "bpm"
-        MenstruationPeriod -> "hours"
+        MenstruationPeriod -> "h"
         OxygenSaturation -> "%"
-        Sleep -> "hours"
+        Sleep -> "h"
         HeartRateVariabilityRmssd -> null
         SkinTemperature -> "°C"
         Symptoms -> null
     }
 
-    // TODO i18n
     @Composable
     fun note(): String = when (this.measurement) {
-        Steps -> "Today"
-        Distance -> "Today"
-        ElevationGained -> "Today"
-        Speed -> "Average Today"
-        HeartRate -> "Average Today"
-        MenstruationPeriod -> "Today"
-        OxygenSaturation -> "Average Today"
-        Sleep -> "Today"
-        HeartRateVariabilityRmssd -> "Today"
-        SkinTemperature -> "Average Today"
-        Symptoms -> "Average Today"
+        Steps -> stringResource(R.string.today)
+        Distance -> stringResource(R.string.today)
+        ElevationGained -> stringResource(R.string.today)
+        Speed -> stringResource(R.string.average_today)
+        HeartRate -> stringResource(R.string.average_today)
+        MenstruationPeriod -> stringResource(R.string.today)
+        OxygenSaturation -> stringResource(R.string.average_today)
+        Sleep -> stringResource(R.string.today)
+        HeartRateVariabilityRmssd -> stringResource(R.string.today)
+        SkinTemperature -> stringResource(R.string.average_today)
+        Symptoms -> stringResource(R.string.average_today)
     }
 }
 
